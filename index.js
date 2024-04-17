58ba8e900254dd96329444356bbdd971b9819fa3
Krishna Manoj PVR <pvrkmsbunny.com>
2024-04-17 13:47:19 +0530

    shapematching

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serveStatic = require("serve-static");
 // const finalHandler = require('finalhandler');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // Serve static files from the 'public' directory
 const staticRouter1 = serveStatic(path.join(__dirname, "ge1"), {
@@ -22,15 +22,23 @@ const staticRouter2 = serveStatic(path.join(__dirname, "ge2"), {
 });
 
 const staticRouter3 = serveStatic(path.join(__dirname, "ge3"), {
-    setHeaders: (res, path) => {
-      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    },
-  });
+  setHeaders: (res, path) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  },
+});
+
+const staticRouter4 = serveStatic(path.join(__dirname, "shapematchinghtml"), {
+  setHeaders: (res, path) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  },
+});
 
 app.use("/", staticRouter1);
 app.use("/", staticRouter2);
 app.use("/", staticRouter3);
+app.use("/", staticRouter4);
 // app.use(finalHandler());
 
 // Set headers to enable Cross-Origin Isolation and SharedArrayBuffer
@@ -65,8 +73,11 @@ app.get("/game2", (req, res) => {
   res.sendFile(path.join(__dirname, "ge2", "DungeonCrawler.html"));
 });
 app.get("/game3", (req, res) => {
-    res.sendFile(path.join(__dirname, "ge3", "Dynamic Split Screen.html"));
-  });
+  res.sendFile(path.join(__dirname, "ge3", "Dynamic Split Screen.html"));
+});
+app.get("/game4", (req, res) => {
+  res.sendFile(path.join(__dirname, "shapematchinghtml", "shapematching.html"));
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
